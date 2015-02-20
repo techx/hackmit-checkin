82f8b02c336d90136a06dfe2beac1ce093b42916
Anish Athalye <me@anishathalye.com>
2015-02-19 22:24:49 -0500

    Add sample

---
+++ web/data.sample.js
@@ -0,0 +1,9 @@
+var personData = [
+    {
+        "account_id": "3",
+        "legal_waiver": "Full Legal Name",
+        "organization": "University Name Here",
+        "badge_name": "Nickname",
+        "dietary_restriction": "none"
+    }
+];

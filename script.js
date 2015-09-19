9df6f48ec02a8aad5537d6e0d02a6d2aa4c82dbd
Edwin Zhang <ehzhang@mit.edu>
2015-09-18 22:08:53 -0400

    okay

--- script.js
+++ script.js
@@ -374,7 +374,7 @@
     userDB.fetchUsers(function(data){
       loadingMessage.addClass('hidden');
       userDB.setUsers(data.filter(function(user){
-        return user.verified && user.status.admitted;
+        return user.verified;
       }));
       updateLastUpdatedTime();
       console.log("Fetched users successfully");
